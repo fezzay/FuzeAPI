62eab817bca8bdb01246c406604c650d99b13f56
fezzay <thomasjccooper@gmail.com>
2024-03-17 21:17:49 +0000

    Added kube get pods basic

--- FuzeAPI/Fuze.Kube.Adapter/KubeRepository.cs
+++ FuzeAPI/Fuze.Kube.Adapter/KubeRepository.cs
@@ -1,17 +1,26 @@
 ﻿using Fuze.Domain.Interfaces;
 using FuzeAPI.Models;
+using k8s;
 
 namespace Fuze.Kube.Adapter
 {
     public class KubeRepository : IKubeRepository
     {
-        public KubeRepository() 
+        private KubernetesClientConfiguration _kubeClientConfig;
+        private Kubernetes _kube;
+        public KubeRepository()
         {
-
+            _kubeClientConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile("/etc/rancher/k3s/k3s.yaml");
+            _kube = new Kubernetes(_kubeClientConfig);
         }
 
         public Pods GetAllPods()
         {
+            var list = _kube.CoreV1.ListNamespacedPod("default");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
             return new Pods()
             {
                 Date = DateTime.Now.AddDays(1),
