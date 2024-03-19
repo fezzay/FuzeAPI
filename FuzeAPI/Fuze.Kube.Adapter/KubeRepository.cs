893c3b63e91536afcc5b8ba2c715a57ed1aa91cf
fezzay <thomasjccooper@gmail.com>
2024-03-19 01:09:36 +0000

    fixed container

--- FuzeAPI/Fuze.Kube.Adapter/KubeRepository.cs
+++ FuzeAPI/Fuze.Kube.Adapter/KubeRepository.cs
@@ -6,21 +6,21 @@ namespace Fuze.Kube.Adapter
 {
     public class KubeRepository : IKubeRepository
     {
-        //private KubernetesClientConfiguration _kubeClientConfig;
-        //private Kubernetes _kube;
+        private KubernetesClientConfiguration _kubeClientConfig;
+        private Kubernetes _kube;
         public KubeRepository()
         {
-            //_kubeClientConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile("/etc/rancher/k3s/k3s.yaml");
-            //_kube = new Kubernetes(_kubeClientConfig);
+            _kubeClientConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            _kube = new Kubernetes(_kubeClientConfig);
         }
 
         public Pods GetAllPods()
         {
-            //var list = _kube.CoreV1.ListNamespacedPod("default");
-            //foreach (var item in list.items)
-            //{
-            //    console.writeline(item.metadata.name);
-            //}
+            var list = _kube.CoreV1.ListNamespacedPod("default");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
             return new Pods()
             {
                 Date = DateTime.Now.AddDays(1),
